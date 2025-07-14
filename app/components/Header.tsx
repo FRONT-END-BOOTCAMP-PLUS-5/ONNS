157d8e9ba0dbc6a3a2e8a6d50be9eb6e3592e441
Shin363 <wngus4595@gmail.com>
2025-07-14 14:01:25 +0900

    fix: merge conflict 해결

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,17 +6,19 @@ import Notification from '@/public/assets/icons/notification-read.svg';
 import NotificationUnread from '@/public/assets/icons/notification_unread.svg';
 import { useRouter } from 'next/navigation';
 
-type RightIconType =
-  | { type: 'notification'; active: boolean; onClick: () => void }
-  | { type: 'edit'; isEditing: boolean; onClick: () => void }
-  | { type: 'close'; onClick: () => void };
-
 interface HeaderProps {
   isGoBack?: boolean;
-  rightIcons?: RightIconType[];
+  isClose?: boolean;
+  isHome?: boolean;
+  hasUnreadNotification?: boolean;
 }
 
-const Header = ({ isGoBack = false, rightIcons = [] }: HeaderProps) => {
+const Header = ({
+  isGoBack = false,
+  isClose = false,
+  isHome = false,
+  hasUnreadNotification = false,
+}: HeaderProps) => {
   const router = useRouter();
   return (
     <div className="w-full pt-[16px] py-[16px] pl-[20px] pr-[20px] bg-white inline-flex justify-between items-center">
@@ -24,37 +26,10 @@ const Header = ({ isGoBack = false, rightIcons = [] }: HeaderProps) => {
         {isGoBack && <Back />}
       </button>
       <Logo width={92} height={29} />
-      <div className="w-[24px] h-[24px] flex items-center justify-center">
-        {rightIcons.map((icon, idx) => {
-          if (icon.type === 'notification') {
-            return (
-              <button key={`notification-${idx}`} onClick={icon.onClick} aria-label="알림">
-                {icon.active ? <NotificationUnread /> : <Notification />}
-              </button>
-            );
-          }
-          if (icon.type === 'edit') {
-            return (
-              <button
-                key={`edit-${idx}`}
-                className="text-[16px] whitespace-nowrap text-[color:var(--b400)] "
-                onClick={icon.onClick}
-                aria-label={icon.isEditing ? '수정' : '완료'}
-              >
-                {icon.isEditing ? '완료' : '수정'}
-              </button>
-            );
-          }
-          if (icon.type === 'close') {
-            return (
-              <button key={`close-${idx}`} onClick={router.back} aria-label="닫기">
-                <Close />
-              </button>
-            );
-          }
-          return null;
-        })}
-      </div>
+      <button className="w-[24px] h-[24px] flex items-center justify-center " onClick={router.back}>
+        {isClose && <Close />}
+        {isHome && (hasUnreadNotification ? <NotificationUnread /> : <Notification />)}
+      </button>
     </div>
   );
 };

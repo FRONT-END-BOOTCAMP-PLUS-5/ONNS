9a2128672df814432f535160f1b5cc761db4e7c4
Shin363 <wngus4595@gmail.com>
2025-07-14 23:16:26 +0900

    feat: 홈 우산지수,마스크지수 컴포넌트 구현

--- app/(anon)/home/HomeClient.tsx
+++ app/(anon)/home/HomeClient.tsx
@@ -3,7 +3,11 @@
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { useLoginModal } from '@/hooks/useLoginModal';
-import { KakaoLoginModalContainer } from '@/app/components';
+import { Header, KakaoLoginModalContainer } from '@/app/components';
+import WeatherIndex from './components/WeatherIndex';
+import { useWeatherStore } from '@/stores/weatherState';
+import { useLocation } from '@/hooks/useLocation';
+import { useWeather } from '@/hooks/useWeather';
 
 export default function HomeClient() {
   const searchParams = useSearchParams();
@@ -14,6 +18,14 @@ export default function HomeClient() {
     const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
     window.location.href = kakaoAuthUrl;
   };
+
+  const { lat, lon } = useLocation();
+  useWeather(lat, lon);
+  // const { cityName, feels_like, umbrellaIndex, dustIndex } = useWeatherStore();
+  const { umbrellaIndex, dustIndex } = useWeatherStore();
+  // const shouldFetch = !!lat && !!lon && feels_like === null;
+  // useWeather(shouldFetch ? lat : null, shouldFetch ? lon : null);
+
   useEffect(() => {
     if (searchParams.get('login') === '1' && !isAuthenticated && !loading) {
       handleOpenModal();
@@ -25,6 +37,8 @@ export default function HomeClient() {
       {showLoginModal && (
         <KakaoLoginModalContainer onLogin={handleLogin} onClose={handleCloseModal} />
       )}
+      <Header isHome={true} hasUnreadNotification={true} />
+      <WeatherIndex umbrellaIndex={umbrellaIndex} dustIndex={dustIndex} />
     </>
   );
 }

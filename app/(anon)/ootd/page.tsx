d3bfbb8acd271dcddf6238369ff8fdec33d6fed0
Hyung <h9564289@gmail.com>
2025-07-15 23:59:01 +0900

    feat: 최신순, 인기순 필터 UI

--- app/(anon)/ootd/page.tsx
+++ app/(anon)/ootd/page.tsx
@@ -1,12 +1,14 @@
 'use client';
-import OotdSeasonDropdown from './components/SeasonFilter/OotdSeasonDropdown';
+import OotdSeasonDropdown from './components/SeasonFlt/OotdSeasonDropdown';
 import TempFlt from './components/TempFlt/TempFlt';
+import SortPost from './components/SortPost/SortPost';
 
 const Ootd = () => {
   return (
     <div className="flex">
       <OotdSeasonDropdown />
       <TempFlt />
+      <SortPost />
     </div>
   );
 };

9437725ebb73724e6db55b9b7cbe47bc7647da8c
Shin363 <wngus4595@gmail.com>
2025-07-15 17:36:06 +0900

    feat: ootd 상세페이지 유저 조회 영역 구현

---
+++ app/(anon)/ootd/[id]/components/PostUserInfo.tsx
@@ -0,0 +1,75 @@
+'use client';
+import Image from 'next/image';
+import More from '@/public/assets/icons/more.svg';
+import { useEffect, useRef, useState } from 'react';
+import { DeletePostModalContainer } from '@/app/components';
+interface PostUserInfoProps {
+  profile_image: string | null;
+  nickname: string | null;
+  isMyPost: boolean | null;
+}
+const PostUserInfo = ({ profile_image, nickname, isMyPost }: PostUserInfoProps) => {
+  const [open, setOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!open) return;
+    const handleClick = (e: MouseEvent) => {
+      if (popoverRef.current && !popoverRef.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, [open]);
+  return (
+    <>
+      <div className="flex justify-between items-center px-4 pt-[8px] pb-[8px]">
+        <div className="flex w-[200px] items-center ">
+          <div className="relative w-[38px] h-[38px] mr-[6px]">
+            <Image
+              src={profile_image || ''}
+              alt="글쓴이 프로필"
+              fill
+              sizes="38px"
+              className="rounded-full bg-neutral-200"
+            />
+          </div>
+          <div className="w-[120px] align-middle text-[14px] font-medium text-sm whitespace-nowrap overflow-hidden text-ellipsis">
+            {nickname}
+          </div>
+        </div>
+        {isMyPost && (
+          <div className="relative" ref={popoverRef}>
+            <button onClick={() => setOpen((v) => !v)}>
+              <More />
+            </button>
+            {open && (
+              <button
+                className="w-[58px] h-[30px] absolute top-5 right-0 border-[#F0EEEE] bg-[#F0EEEE] rounded-[10px] text-[12px] font-light"
+                onClick={() => {
+                  setOpen(false);
+                  setShowModal(true);
+                }}
+              >
+                삭제하기
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+      {showModal && (
+        <DeletePostModalContainer
+          onDelete={() => {
+            // 게시글 삭제 API 호출
+            setShowModal(false);
+          }}
+          onClose={() => setShowModal(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default PostUserInfo;
--- app/(anon)/ootd/[id]/page.tsx
+++ app/(anon)/ootd/[id]/page.tsx
@@ -1,6 +1,27 @@
 'use client';
 
+import PostUserInfo from '@/app/(anon)/ootd/[id]/components/PostUserInfo';
+import Image from 'next/image';
+import Heart from '@/public/assets/icons/heart.svg';
+import StrokeHeart from '@/public/assets/icons/stroke_heart.svg';
+import StrokeComment from '@/public/assets/icons/stroke_comment.svg';
+
 //ootd detail
 export default function OotdDetail() {
-  return <></>;
+  // 프로필 이미지, 닉네임
+  // 게시글 이미지, 텍스트, 좋아요 수, 댓글 수, 내 포스트인지 여부
+  // 댓글 작성자 프로필 이미지, 닉네임, 댓글 내용, 내 댓글인지 여부
+
+  return (
+    <>
+      <PostUserInfo profile_image={''} nickname={'주현주현주현주현주현주현주현'} isMyPost={true} />
+      <Image src={''} alt="게시글 이미지" className="w-full h-[429px] bg-neutral-200" />
+      <div className="flex ml-[20px] mr-[20px] pt-[12px] pb-[12px]"></div>
+      <div className="flex flex-row">
+        <Heart className="w-[24px] h-[24px]" />
+        <StrokeHeart className="w-[24px] h-[24px]" />
+        <StrokeComment className="w-[24px] h-[24px]" />
+      </div>
+    </>
+  );
 }

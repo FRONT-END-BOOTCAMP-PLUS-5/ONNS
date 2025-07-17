1dede2cdcb4da8c85086e212857fa1ebc2395bc1
shinjuhyeon <wngus4595@gmail.com>
2025-07-17 22:53:17 +0900

    feat: 홈 ootd 인기 게시글 조회 프론트 연결

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -44,7 +44,7 @@ const Button: React.FC<ButtonProps> = ({
 const MoreButton: React.FC<MoreButtonProps> = ({ content, onClick }) => {
   return (
     <div
-      className="flex items-center mx-[20px] w-[390px] h-[52px] justify-center rounded-[26px] border-[1.50px] shadow-[0px_0px_12px_0px_rgba(30,30,30,0.32)]"
+      className="flex items-center mx-[20px] w-[390px] h-[52px] justify-center rounded-[26px] border-[1.50px] "
       onClick={onClick}
     >
       <button className="font-medium text-lg">

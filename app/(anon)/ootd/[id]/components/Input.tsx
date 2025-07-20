b86afd6b0bd2040ff2f2c4dbaf1f1e75645d43a3
shinjuhyeon <wngus4595@gmail.com>
2025-07-20 22:53:25 +0900

    fix: iOS 환경에서 인풋 포커스 시 확대되는 현상 수정

--- app/(anon)/ootd/[id]/components/Input.tsx
+++ app/(anon)/ootd/[id]/components/Input.tsx
@@ -11,14 +11,14 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({ value, onChange, onSend, inputRef }) => {
   const isEmpty = !value.trim();
   return (
-    <div className="max-w-[430px] w-full mx-auto fixed bottom-0 left-1/2 -translate-x-1/2 bg-white z-50 pb-[36px]">
+    <div className="max-w-[430px] w-full mx-auto fixed bottom-0 left-1/2 -translate-x-1/2 bg-white z-200 pb-[36px]">
       {/* 상단 구분선 */}
       <div className="h-px bg-gray-100 mb-4 " />
       <div className="flex items-center h-11 ml-4 mr-4">
         <input
           type="text"
           ref={inputRef}
-          className="flex-1 h-full bg-[#f1f0f0] rounded-l-xl rounded-r-xl px-4 py-2 text-[15px] text-black placeholder-gray-400 border-none outline-none focus:ring-0"
+          className="flex-1 h-full bg-[#f1f0f0] rounded-l-xl rounded-r-xl px-4 py-2 text-[16px] text-black placeholder-gray-400 border-none outline-none focus:ring-0"
           placeholder={'댓글을 입력해주세요.'}
           value={value}
           onChange={onChange}
